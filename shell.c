db84c4a3295bb2bc57b5c5ed399c65ffe29c6936
Sam-G-Birhanu <110263254+Sam-G-Birhanu@users.noreply.github.com>
2023-09-20 22:47:02 +0530

    Create shell.c

---
+++ shell.c
@@ -0,0 +1,12 @@
+#include "shell.h"
+/**
+ * main - a function that executes other programs
+ * Return: Exit status
+ */
+char *ORIGINAL_PATH = NULL;
+int main()
+{
+	ORIGINAL_PATH = getenv("PATH");
+	prompt();
+	return 0;
+}
