1f8a6637cfbf30adccadf8f1a110be91e6a3e699
Sam-G-Birhanu <21btrcs@jainuniversity.ac.in>
2023-09-20 13:23:07 -0700

    m

--- executeCommand.c
+++ executeCommand.c
@@ -2,37 +2,38 @@
 
 extern char **environ;
 extern char *ORIGINAL_PATH;
-int executeCommand(char **tokens) {
+int executeCommand(char **tokens)
+{
 	int result;
 	char *programName;
 	char *token;
 	char *original_path = strdup(ORIGINAL_PATH);
-	if (strcmp(tokens[0], "exit") == 0) {
+	if (strcmp(tokens[0], "exit") == 0)
+	{
 		exit(0);
 	}
-
-
 	result = handleBuiltins(tokens);
-
-	if (result != -1) {
+	if (result != -1)
+	{
 		setenv("PATH", original_path, 1);
-		return result;
+		return (result);
 	}
-
-
 	programName = tokens[0];
 	token = strtok(original_path, ":");
 	if (programName[0] == '/')
 	{
-		if (fileExists(programName)) {
+		if (fileExists(programName))
+		{
 		    executePathSpecifiedCmd(programName, tokens);
-		    return -1;
+		    return (-1);
 		}
 	}
 	while (token != NULL)
 	{
 		char *programPath = malloc(strlen(token) + strlen("/") + strlen(programName) + 1);
-		if (programPath == NULL) {
+
+		if (programPath == NULL)
+		{
 			perror("malloc");
 			exit(EXIT_FAILURE);
 		}
@@ -42,47 +43,45 @@ int executeCommand(char **tokens) {
 		strcat(programPath, programName);
 
 
-		if (fileExists(programPath)) {
+		if (fileExists(programPath))
+		{
 			pid_t child = fork();
 
-			if (child == -1) {
+			if (child == -1)
+			{
 				perror("fork");
 				exit(EXIT_FAILURE);
 			}
-
-			if (child == 0) {
-
-
+			if (child == 0)
+			{
 				char *args[MAX_ARGUMENTS];
-				int i; 
+				int i;
+
 				setenv("PATH", original_path, 1);
 
 				args[0] = programPath;
-
-
-				for (i = 1; tokens[i] != NULL; i++) {
+				for (i = 1; tokens[i] != NULL; i++)
+				{
 					args[i] = tokens[i];
 				}
-
 				args[i] = NULL;
-
 				execve(programPath, args, environ);
 				free(programPath);
 				perror(".hsh");
 				exit(EXIT_FAILURE);
-			} else {
+			}
+			else
+			{
 				int status;
-				wait(&status);
 
+				wait(&status);
 				free(programPath);
-				return 0;
+				return (0);
 			}
 		}
-
 		free(programPath);
 		token = strtok(NULL, ":");
 	}
-
 	perror(".hsh");
-	return -1;
+	return (-1);
 }
--- handle_built_ins.c
+++ handle_built_ins.c
@@ -7,13 +7,16 @@
 
 int handleBuiltins(char **tokens)
 {
-	if (tokens[0] == NULL) {
-		return 0;
+	if (tokens[0] == NULL)
+	{
+		return (0);
 	}
 
-	if (strcmp(tokens[0], "exit") == 0) {
+	if (strcmp(tokens[0], "exit") == 0)
+	{
 		exit(0);
-	} else if (strcmp(tokens[0], "cd") == 0)
+	}
+	else if (strcmp(tokens[0], "cd") == 0)
 	{
 		if (tokens[1] != NULL)
 		{
@@ -25,23 +28,29 @@ int handleBuiltins(char **tokens)
 		else
 		{
 			struct passwd *pw = getpwuid(getuid());
-			if (pw != NULL) {
-				if (chdir(pw->pw_dir) == -1) {
+			if (pw != NULL)
+			{
+				if (chdir(pw->pw_dir) == -1)
+				{
 					perror("chdir");
 				}
 			}
 		}
-		return 0;
+		return (0);
 	}
-	else if (strcmp(tokens[0], "pwd") == 0) {
+	else if (strcmp(tokens[0], "pwd") == 0)
+	{
 		char cwd[1024];
-		if (getcwd(cwd, sizeof(cwd)) != NULL) {
+		if (getcwd(cwd, sizeof(cwd)) != NULL)
+		{
 			printf("%s\n", cwd);
-		} else {
+		} else
+		{
 			perror("getcwd");
 		}
-		return 0;
+		return (0);
 	}
 
-	return -1;
+	return
+		(-1);
 }
