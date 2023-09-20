7e660bb793f57f470cd6758d1730e87069e04aa9
Sam-G-Birhanu <110263254+Sam-G-Birhanu@users.noreply.github.com>
2023-09-21 01:02:51 +0530

    Update shell.c

--- shell.c
+++ shell.c
@@ -3,10 +3,9 @@
  * main - a function that executes other programs
  * Return: Exit status
  */
+char *ORIGINAL_PATH;
 int main()
 {
-	/** char *ORIGINAL_PATH;
-	ORIGINAL_PATH = getenv("PATH"); **/
 	prompt();
 	return 0;
 }
