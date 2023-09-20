18097e5c4aa52f7917b503e089ad9f7ce7fb31fa
Sam-G-Birhanu <110263254+Sam-G-Birhanu@users.noreply.github.com>
2023-09-20 18:18:28 +0530

    Update fileExists.c

--- fileExists.c
+++ fileExists.c
@@ -7,7 +7,7 @@
 
 int fileExists(const char *filename) {
     char *path = getenv("PATH");
-   /** printf("%s", path); **/
+    printf("%s \n", path);
     if (access(filename, F_OK) == 0) {
         return 1;  
     } else {
