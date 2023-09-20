e9e7801d82ce7e2bb8d81996f93e31656f7ea85e
Sam-G-Birhanu <110263254+Sam-G-Birhanu@users.noreply.github.com>
2023-09-20 17:28:18 +0530

    Update shell.c: passed path to prompt

--- shell.c
+++ shell.c
@@ -3,12 +3,12 @@
  * main - a function that executes other programs
  * Return: Exit status
  */
-char *ORIGINAL_PATH = NULL;
+/** char *ORIGINAL_PATH = NULL; **/
 int main()
 {
-	ORIGINAL_PATH = getenv("PATH");
+	const char *ORIGINAL_PATH = getenv("PATH");
 	printf("%s original path" , ORIGINAL_PATH);
-	prompt();
+	prompt(ORIGINAL_PATH);
 	return 0;
 }
 
