18ebda11c88efd498e31f40f0dbd3e343c0185cd
Sam-G-Birhanu <110263254+Sam-G-Birhanu@users.noreply.github.com>
2023-09-22 14:27:33 +0530

    Create _puts.c

---
+++ _puts.c
@@ -0,0 +1,16 @@
+#include "shell.h"
+
+/**
+ * _puts - Prints a string to standard output
+ * @str: String to be printed
+ *
+ * Return: No of chars printed
+*/
+int _puts(char *strng)
+{
+	int leng = _strlen(strng);
+
+	write(STDOUT_FILENO, strng, leng);
+
+	return (leng);
+}
