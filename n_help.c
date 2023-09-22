d71260f68e1b566c5f94d9702748b136c769e2d0
Sam-G-Birhanu <110263254+Sam-G-Birhanu@users.noreply.github.com>
2023-09-22 16:39:29 +0530

    Update n_help.c

--- n_help.c
+++ n_help.c
@@ -23,11 +23,11 @@ int fun_read_line(const int f_d, char **line)
 			str[f_d] = _strdup(buffer);
 		else
 		{
-			tmp = f_strjoin(str[f_d], buffer);
+			tmp = str_join(str[f_d], buffer);
 			free(str[f_d]);
 			str[f_d] = tmp;
 		}
-		if (_strchr(str[f_d], '\n'))
+		if (str_chr(str[f_d], '\n'))
 			break;
 	}
 	if (str[f_d] == NULL && reader == 0)
@@ -50,17 +50,17 @@ int	new_read_line(char **str, char **line, int f_d)
 	count = 0;
 	while (str[f_d][count] != '\0' && str[f_d][count] != '\n')
 		count++;
-	*line = f_strsub(str[f_d], 0, count);
+	*line = str_sub(str[f_d], 0, count);
 	if (str[f_d][count] == '\n')
 	{
 		tmp = _strdup(&str[f_d][count + 1]);
 		free(str[f_d]);
 		str[f_d] = tmp;
 		if (str[f_d][0] == '\0')
-			f_strdel(&str[f_d]);
+			str_del(&str[f_d]);
 	}
 	else
-		f_strdel(&str[f_d]);
+		str_del(&str[f_d]);
 	return (1);
 }
 
@@ -83,7 +83,7 @@ int fun_help(char **commands)
 			result = 0;
 			while (fun_read_line(f_d, &line) == 1)
 			{
-				puts(line);
+				_puts(line);
 				free(line);
 			}
 			close(f_d);
