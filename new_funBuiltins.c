d8cf65232b4bf996647f0cd9292c749df87bd17b
Sam-G-Birhanu <110263254+Sam-G-Birhanu@users.noreply.github.com>
2023-09-23 13:25:24 +0530

    Update new_funBuiltins.c

--- new_funBuiltins.c
+++ new_funBuiltins.c
@@ -1,7 +1,7 @@
 #include "shell.h"
 
 /**
- * get_env_var_index - Check if an environment variable exists 
+ * get_env_var_index - Check if an environment variable exists
  * @env_name: Name to be searched
  *
  * Return: Index of the environment variable, -1 on error
